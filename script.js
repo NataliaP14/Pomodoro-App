29b848da3493c67e2e1879d691477bfe3a75d5f9
Natalia Peguero <pegueronatalia@gmail.com>
2024-04-06 23:12:16 -0400

    fixed the stop timer

--- script.js
+++ script.js
@@ -3,46 +3,47 @@ const stopButton = document.querySelector(".button-stop");
 const session = document.querySelector(".minutes");
 let interval;
 let state = true;
-let totalSeconds;
+let totalSeconds = 25 * 60;
 let remainingSeconds;
 
 
 const appTimer = () => {
-    const sessionAmount = Number.parseInt(session.textContent);
+   
 
     if (state) {
         state = false;
-        totalSeconds = sessionAmount * 60;
-
-        const updateSeconds = () => {
-            const minuteDiv = document.querySelector('.minutes');
-            const secondDiv = document.querySelector('.seconds');
-
-            totalSeconds--;
-
-            let minutesLeft = Math.floor(totalSeconds/60);
-            let secondsLeft = totalSeconds % 60;
-
-            if (secondsLeft < 10) {
-                secondDiv.textContent = "0" + secondsLeft;
-
-            } else {
-                secondDiv.textContent = secondsLeft;
-            }
-            minuteDiv.textContent = minutesLeft;
-
-            if (minutesLeft === 0 && secondsLeft === 0) {
-
-                clearInterval(interval);
-            } 
-        }
-        
         clearInterval(interval);
         interval = setInterval(updateSeconds, 1000);
     } else {
         alert("Session has already started.");
-    }
+    } 
 }
+
+const updateSeconds = () => {
+    const minuteDiv = document.querySelector('.minutes');
+    const secondDiv = document.querySelector('.seconds');
+
+    totalSeconds--;
+
+    let minutesLeft = Math.floor(totalSeconds/60);
+    let secondsLeft = totalSeconds % 60;
+
+     if (secondsLeft < 10) {
+        secondDiv.textContent = "0" + secondsLeft;
+
+    } else {
+        secondDiv.textContent = secondsLeft;
+    }
+    minuteDiv.textContent = minutesLeft;
+
+    if (minutesLeft === 0 && secondsLeft === 0) {
+
+        clearInterval(interval);
+    } 
+}
+        
+      
+
 
 const stopTimer = () => {
     
