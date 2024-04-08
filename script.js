41bbfa5b18acab75d0cec946ea6cce0f7eacb21a
Natalia Peguero <pegueronatalia@gmail.com>
2024-04-07 23:29:01 -0400

    added a settings button with a GUI

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const stopButton = document.querySelector(".button-stop");
 const resetButton = document.querySelector(".button-reset");
 const session = document.querySelector(".minutes");
 const bells = new Audio("./assets/bell.wav");
+const openSettingsButtons = document.querySelectorAll("[data-setting-target]");
+const closeSettingsButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 let interval;
 let state = true;
 let totalSeconds = 25 * 60;
@@ -55,6 +58,34 @@ const resetTimer = () => {
     updateSeconds();
 }
 
+openSettingsButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const settings = document.querySelector(button.dataset.settingTarget);
+        openSettings(settings);
+    })
+})
+
+closeSettingsButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const settings = button.closest(".settings");
+        closeSettings(settings);
+    })
+})
+
+function openSettings(settings) {
+    if (settings == null) 
+        return
+    
+    settings.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeSettings(settings) {
+    if (settings == null) return
+        
+    settings.classList.remove("active");
+    overlay.classList.remove("active");
+}
 
 startButton.addEventListener("click", appTimer);
 stopButton.addEventListener("click", stopTimer);
