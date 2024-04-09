9e00c2be41878fa5ffafcf83b12e978f24ab168f
Natalia Peguero <pegueronatalia@gmail.com>
2024-04-08 21:07:07 -0400

    added logic for custom timer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector(".button-start");
 const stopButton = document.querySelector(".button-stop");
 const resetButton = document.querySelector(".button-reset");
+const customButton = document.getElementById("customButton");
 const session = document.querySelector(".minutes");
 const bells = new Audio("./assets/bell.wav");
 const openSettingsButtons = document.querySelectorAll("[data-setting-target]");
@@ -11,7 +12,7 @@ let state = true;
 let totalSeconds = 25 * 60;
 let remainingSeconds;
 
-
+//app timer when you press start
 const appTimer = () => {
     if (state) {
         state = false;
@@ -21,7 +22,7 @@ const appTimer = () => {
         alert("Session has already started.");
     } 
 }
-
+//logic for updating the seconds
 const updateSeconds = () => {
     const minuteDiv = document.querySelector('.minutes');
     const secondDiv = document.querySelector('.seconds');
@@ -44,13 +45,13 @@ const updateSeconds = () => {
         clearInterval(interval);
     } 
 }
-        
+//stops the timer      
 const stopTimer = () => {
     clearInterval(interval);
     state = true;
     
 }
-
+//resets the timer
 const resetTimer = () => {
     clearInterval(interval);
     totalSeconds = 25 * 60 + 1;
@@ -58,13 +59,34 @@ const resetTimer = () => {
     updateSeconds();
 }
 
+//logic for if user wants to input a custom time
+const customTimeInput = () => {
+    const customMin = parseInt(document.getElementById("customMin").value);
+    const customSec = parseInt(document.getElementById("customSec").value);
+    if (customMin >= 0 && customMin <= 500 && customSec >= 0 && customSec <= 59) {
+        customButton.disabled = false;
+    } else {
+        customButton.disabled = true;
+    }
+}
+//logic for when user submits their custom time, gets updates
+const customTime = () => {
+    clearInterval(interval);
+     customMin = parseInt(document.getElementById("customMin").value);
+     customSec = parseInt(document.getElementById("customSec").value);
+     totalSeconds = customMin * 60 + customSec + 1;
+     state = true;
+     updateSeconds();
+}
+
+//logic for when the user clicks on the settings button
 openSettingsButtons.forEach(button => {
     button.addEventListener("click", () => {
         const settings = document.querySelector(button.dataset.settingTarget);
         openSettings(settings);
     })
 })
-
+//logic for when the user closes the settings button
 closeSettingsButtons.forEach(button => {
     button.addEventListener("click", () => {
         const settings = button.closest(".settings");
@@ -87,7 +109,11 @@ function closeSettings(settings) {
     overlay.classList.remove("active");
 }
 
+
+//event listeners
 startButton.addEventListener("click", appTimer);
 stopButton.addEventListener("click", stopTimer);
 resetButton.addEventListener("click", resetTimer);
-
+customMin.addEventListener("input", customTimeInput);
+customSec.addEventListener("input", customTimeInput);
+customButton.addEventListener("click", customTime);
